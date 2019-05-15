0b701dadcd282dc8c67a8e04d382d0626e900509
Bry Cox <carl.cc@gmail.com>
2019-05-15 19:15:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student < InteractiveRecord
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
-    
+
     values.join(", ")
     binding.pry
 
